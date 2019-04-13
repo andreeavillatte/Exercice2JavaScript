de5e34dc9ece80906347db43bb6b38f7e7c59d0e
core.editor <andreeavillatte@gmail.com>
2019-04-13 15:05:14 +0200

    après correction

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Je declare trois variables sur une seule lignie et leur donner des valeurs
+let lastName='Doe', firstName='John', city='New-York';
+
+//J'affiche les trois variables avec ALERT plus les trois variables et j'ajoute le text a etre affiché dans des cotes simples
+// dans le js j'utilise les cotes simple ' ' et pas les cotest doubles " "
+// Je concatene les trois variables avec +
+// pour soter a la ligne j'utilise \n
+alert('Nom : ' + lastName+ '\nPrénom : ' + firstName + '\nCity : ' + city);
